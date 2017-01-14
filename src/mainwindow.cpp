4c1e35a12cc74f3d7d271d049793df6fc0e9a88b
Eugene Melnik <jeka7js@gmail.com>
2017-01-14 14:26:21 +0200

    Mainwindow minimal class and ui

---
+++ src/mainwindow.cpp
@@ -0,0 +1,56 @@
+/*************************************************************************************************
+ *                                                                                                *
+ *  file: mainwindow.cpp                                                                          *
+ *                                                                                                *
+ *  SpendingControl                                                                               *
+ *  Copyright (C) 2017 Eugene Melnik <jeka7js@gmail.com>                                          *
+ *                                                                                                *
+ *  SpendingControl is free software; you can redistribute it and/or modify it under the terms of *
+ *  the GNU General Public License as published by the Free Software Foundation; either version 2 *
+ *  of the License, or (at your option) any later version.                                        *
+ *                                                                                                *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;     *
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.     *
+ *  See the GNU General Public License for more details.                                          *
+ *                                                                                                *
+ *  You should have received a copy of the GNU General Public License along with this program.    *
+ *  If not, see <http://www.gnu.org/licenses/>.                                                   *
+ *                                                                                                *
+  *************************************************************************************************/
+
+#include "defines.h"
+#include "mainwindow.h"
+#include "version.h"
+
+
+MainWindow::MainWindow( QWidget* parent ) : QMainWindow( parent )
+{
+    this->setupUi( this );
+    this->setAttribute( Qt::WA_QuitOnClose );
+
+    this->setupMenubar();
+    this->setupToolbar();
+
+    #ifdef QT_DEBUG
+        this->setWindowTitle( " -= DEVELOPMENT BUILD =- version " + Application::appVersionFull );
+    #endif
+}
+
+
+void MainWindow::closeEvent( QCloseEvent* event )
+{
+    emit this->aboutToClose();
+    event->accept();
+}
+
+
+void MainWindow::setupMenubar()
+{
+    this->connect( this->action_FileExit, SIGNAL(triggered()), this, SLOT(close()) );
+}
+
+
+void MainWindow::setupToolbar()
+{
+    this->toolbarMain->addAction( tr( "Exit" ), this, SLOT(close()) );
+}
---
+++ src/mainwindow.h
@@ -0,0 +1,49 @@
+/*************************************************************************************************
+ *                                                                                                *
+ *  file: mainwindow.h                                                                            *
+ *                                                                                                *
+ *  SpendingControl                                                                               *
+ *  Copyright (C) 2017 Eugene Melnik <jeka7js@gmail.com>                                          *
+ *                                                                                                *
+ *  SpendingControl is free software; you can redistribute it and/or modify it under the terms of *
+ *  the GNU General Public License as published by the Free Software Foundation; either version 2 *
+ *  of the License, or (at your option) any later version.                                        *
+ *                                                                                                *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;     *
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.     *
+ *  See the GNU General Public License for more details.                                          *
+ *                                                                                                *
+ *  You should have received a copy of the GNU General Public License along with this program.    *
+ *  If not, see <http://www.gnu.org/licenses/>.                                                   *
+ *                                                                                                *
+  *************************************************************************************************/
+
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QCloseEvent>
+#include <QMainWindow>
+
+#include "ui_mainwindow.h"
+
+
+class MainWindow : public QMainWindow, protected Ui::MainWindow
+{
+    Q_OBJECT
+
+    public:
+        explicit MainWindow( QWidget* parent = nullptr );
+
+    protected slots:
+        void closeEvent( QCloseEvent* event );
+
+    signals:
+        void aboutToClose();
+
+    protected:
+        void setupMenubar();
+        void setupToolbar();
+};
+
+
+#endif // MAINWINDOW_H
